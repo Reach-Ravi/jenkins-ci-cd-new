d6cd4e5c8528048e07cc91b760666a5a87312e50
Reach-Ravi <ivar.kumar7@gmail.com>
2026-02-16 22:01:40 +0530

    first commit

---
+++ src/main/java/com/example/jenkinscicdnew/JenkinsCiCdNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinscicdnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCiCdNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCiCdNewApplication.class, args);
+    }
+
+}
